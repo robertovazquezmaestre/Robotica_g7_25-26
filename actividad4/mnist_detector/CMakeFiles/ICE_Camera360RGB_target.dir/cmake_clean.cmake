7ac3727bcc81ab10c47ddca2f770a5feb3f39b73
robertovazquezmaestre <rvazquezqj@alumnos.unex.es>
2025-12-15 16:30:39 +0100

    estructura del proyecto cambiada

---
+++ actividad4/mnist_detector/CMakeFiles/ICE_Camera360RGB_target.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ICE_Camera360RGB_target"
+  "generated/Camera360RGB.ice"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ICE_Camera360RGB_target.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
